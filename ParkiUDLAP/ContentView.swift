391031965c9a2a72dd0c5e62f45a18b8c9fcf86b
Aldo Serrano <aldo.serranoro@udlap.mx>
2024-04-18 15:09:05 -0600

    Initial Commit

---
+++ ParkiUDLAP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkiUDLAP
+//
+//  Created by Aldo Serrano Rugerio on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkiUDLAP/ParkiUDLAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkiUDLAPApp.swift
+//  ParkiUDLAP
+//
+//  Created by Aldo Serrano Rugerio on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkiUDLAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
