1e34268c54e1b31831252dcad3e9414d2e70ae3f
Aldo-Serrano-UDLAP <aldo.serranoro@udlap.mx>
2024-04-18 15:40:48 -0600

    Holaaaa

--- ParkiUDLAP/Views/LogInView.swift
+++ ParkiUDLAP/Views/LogInView.swift
@@ -23,7 +23,7 @@ struct LogInView: View
                     .foregroundStyle(.red)
                     .frame(width: 100, height: 100)
                 Text("Hola tonotos!")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .font(.title)
             }
         }
     }
